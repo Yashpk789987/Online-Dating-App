fd92da84c5bd2315da9a88bdc55855b97caf914b
yash <yashpk789987@gmail.com>
2020-02-22 14:10:59 +0530

    profile picture done message chat started

---
+++ messagesData.js
@@ -0,0 +1,28 @@
+const data = [
+  {_id: '19ff1d4b-ae51-41f2-aa2d-d83b37a26606', createdAt: '2020-02-22T08:27:34.834Z', text: 'Bver', user: {_id: 1}},
+  {_id: '27a42f98-6983-4d90-9b54-ad1ff8e39f57', createdAt: '2020-02-22T08:27:24.129Z', text: 'Egebrb', user: {_id: 1}},
+  {
+    _id: '9aae310c-0a69-4e33-976c-f3539ec52d8a',
+    createdAt: '2020-02-22T08:27:14.537Z',
+    text: 'Hii',
+    user: {_id: '3', name: 'prashant2128'},
+  },
+  {
+    _id: '92d62557-191f-4a72-b714-36c53f5bd1de',
+    createdAt: '2020-02-22T08:27:23.520Z',
+    text: '😁😁😁',
+    user: {_id: '4', name: 'prashant2128'},
+  },
+  {
+    _id: 'd0af1b75-ae05-45b9-914a-002f75c2856c',
+    createdAt: '2020-02-22T08:27:34.873Z',
+    text: 'Dgdgdh',
+    user: {_id: '3', name: 'prashant2128'},
+  },
+  {
+    _id: 'e30a26e7-bce2-433d-af8c-f4e3cce7f705',
+    createdAt: '2020-02-22T08:27:41.157Z',
+    text: 'Jcjc',
+    user: {_id: '3', name: 'prashant2128'},
+  },
+];
